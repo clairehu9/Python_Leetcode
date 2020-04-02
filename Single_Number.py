8de5940b63d37a280ef95052ad2a236eb8122489
Claire Hu <claire.hu9@gmail.com>
2020-04-02 00:01:57 -0700

    Create Single_Number.py

---
+++ Single_Number.py
@@ -0,0 +1,31 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] not in dict:
+                dict[nums[i]] = 1
+            else:
+                dict[nums[i]]+=1
+        for k in dict:
+            if dict[k]==1:
+                return k
+
+
+"" Solution: Hash table:""
+
+from collections import defaultdict
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_table = defaultdict(int)
+        for i in nums:
+            hash_table[i] += 1
+        
+        for i in hash_table:
+            if hash_table[i] == 1:
+                return i
+                
+Complexity Analysis
+
+Time complexity : O(n \cdot 1) = O(n)O(n⋅1)=O(n). Time complexity of for loop is O(n)O(n). Time complexity of hash table(dictionary in python) operation pop is O(1)O(1).
+
+Space complexity : O(n)O(n). The space required by hash\_tablehash_table is equal to the number of elements in \text{nums}nums.
